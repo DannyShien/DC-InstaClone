581b39434f01e0157ed4b18b32a914baa71b4c37
Stanley Chen <dannyshien@gmail.com>
2018-10-15 13:39:47 -0400

    displayed all photos

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
 // ==== ==== InstaClone ==== ====
-const IMAGE = [
-   "images/clear-sky.jpg",
-   "images/clear-sky2.jpg",
-   "images/in-the-clouds.jpg",
-   "images/in-the-sky.jpg",
-   "images/lightning-clouds.jpg",
-   "images/mountain-view-clouds.jpg",
-   "images/ocean-sunset.jpg",
-   "images/over-cloud-sunset.jpg",
-   "images/pink-clouds.jpg",
-   "images/stormy-clouds.jpg",
-   "images/stormy-clouds2.jpg",
+const IMAGES = [
+   {pics:"images/clear-sky.jpg", alt: " "},
+   {pics:"images/clear-sky2.jpg", alt: " "},
+   {pics:"images/in-the-clouds.jpg", alt: " "},
+   {pics:"images/in-the-sky.jpg", alt: " "},
+   {pics:"images/lightning-clouds.jpg", alt: " "},
+   {pics:"images/mountain-view-clouds.jpg", alt: " "},
+   {pics:"images/ocean-sunset.jpg", alt: " "},
+   {pics:"images/over-cloud-sunset.jpg", alt: " "},
+   {pics:"images/pink-clouds.jpg", alt: " "},
+   {pics:"images/stormy-clouds.jpg", alt: " "},
+   {pics:"images/stormy-clouds2.jpg", alt: " "},
 ];
 
 // Alternative version if you're using images on the hard drive.
@@ -19,41 +19,55 @@ const IMAGE = [
 
 // ];
 
+const outputElement = document.querySelector('[data-output]');
+
 // function that generates  an img element
-function createImage(image) {
+function createImage(imageInfo) {
     const theImage = document.createElement('img');
 
-    // theImage.src = imageFile;
-    theImage.setAttribute('src', imageFile); 
+    // theImage.src = imageInfo;
+    // debugger
+    theImage.setAttribute('src', imageInfo.pics); 
+    theImage.setAttribute('alt', imageInfo.alt);
 
     // add an evenListener to the image
     theImage.addEventListener('click', function (event) {
-        console.log('Clicked image');
+        console.log('image clicked');
         // the element that got clicked is accessible
         // as `event.target`
         // And, I can read the `src` attribute!
         console.log(event.target.src);
+        
         // I can now set the output image's src
         // to event.target.src!
+        outputElement.setAttribute('src', event.target.src);
     });
 
     return theImage;
 }
 
 // function that generates the thumbnail div
-function createThumbnail(imageFile) {
+function createThumbnail(imageInfo) {
     const theContainer = document.createElement('div');
     theContainer.classList.add('thumbnail-item');
 
-    const image = createImage(imageFile);
+    const image = createImage(imageInfo);
     theContainer.appendChild(image);
 
     return theContainer;
 }
 
 // just draw a thumbnail to the body
-// so we can test the clicky mc clickerOnAbility
+// so we can test the clicky mc-clickerOnAbility
+// let firstImageFile = IMAGES[0];
 
-let firstImageFile = IMAGES[0];
-let testThumb = createThumbnail(firstImageFile);
-document.body.appendChild(testThumb);
+// Loop through the IMAGES array.
+// For each image, call the anoymous function. 
+// For anon func should except to rceive an image
+IMAGES.forEach(function (anImageInfo) {
+    // We pass the image to our createThumbnail func. 
+    let testThumb = createThumbnail(anImageInfo);
+
+    // then append that thumbnail to the page. 
+    document.body.appendChild(testThumb);
+}); 
